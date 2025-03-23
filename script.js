eee1de3338afaaa7a3017835db7d3fb7912027a5
Aryaman <aryaman.resumepro@gmail.com>
2025-03-24 01:51:01 +0530

    Added logic for hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.getElementById("navbar");
     const faqQuestions = document.querySelectorAll(".faq-question");
+    const mobileMenuButton = document.getElementById("mobileMenuButton");
+    const mobileMenu = document.getElementById("mobileMenu");
 
+    mobileMenuButton.addEventListener("click", (e) => {
+        e.preventDefault(); // Prevent default behavior
+        console.log("Button clicked"); // Debugging: Check if this logs in the console
+
+        // Toggle the mobile menu visibility
+        mobileMenu.classList.toggle("hidden");
+    });
     // Add scrolling effect on navbar
     window.addEventListener("scroll", function () {
         if (window.scrollY > 50) {
